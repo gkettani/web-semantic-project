ee8d0a41def311db2fd007eb3c77bd12ee2e4bee
HubNBr <79989810+HubNBr@users.noreply.github.com>
2022-11-14 11:45:08 +0100

    Tests - Pb same origin

--- src/client/index.js
+++ src/client/index.js
@@ -1,31 +1,45 @@
-import webService from './request.js';
+let searchTerm;
+let div;
 
-let div = document.querySelector('#container');
-let btn = document.querySelector('#btn');
-  
+window.addEventListener('load',(event) => {
+  searchTerm = document.getElementById('search-input');
+  div = document.querySelector('#container');
+});
+
 let query = '';
-/* Query example */
-query = `
-SELECT * WHERE {
-  ?country a dbo:Country; dbo:populationTotal ?population; rdfs:label ?label.
-  FILTER(?population > 15000000 && langMatches(lang(?label), "EN"))
+const prefix = `PREFIX owl: <http://www.w3.org/2002/07/owl#>
+PREFIX xsd: <http://www.w3.org/2001/XMLSchema#>
+PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
+PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
+PREFIX foaf: <http://xmlns.com/foaf/0.1/>
+PREFIX dc: <http://purl.org/dc/elements/1.1/>
+PREFIX : <http://dbpedia.org/resource/>
+PREFIX dbpedia2: <http://dbpedia.org/property/>
+PREFIX dbpedia: <http://dbpedia.org/>
+PREFIX skos: <http://www.w3.org/2004/02/skos/core#>
+\n`;
+
+const basic_request_head = `SELECT ?name WHERE {
+  ?dish a dbo:Food; dbp:name ?name; dbo:thumbnail ?img.
+  FILTER(regex(?name, "`;
+const basic_request_queue = `"))}`;
+
+function search() {
+  query = prefix.concat(basic_request_head, searchTerm.value, basic_request_queue);
+  render();
 }
-ORDER BY DESC(?population)
-LIMIT 50`;
 
 function render() {
-  webService
-    .request(query)
+  request(query)
     .then((response) => {
       response.results.bindings.forEach((item) => {
-        let p = document.createElement('p');
+        console.log(p);
+        /*let p = document.createElement('p');
         p.innerHTML = item.label.value;
-        div.appendChild(p);
+        div.appendChild(p);*/
       });
     })
     .catch((error) => {
       console.log(error);
     });
 }
-
-btn.addEventListener('click', render);
