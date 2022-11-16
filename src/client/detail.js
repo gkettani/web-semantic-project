c00462d55d55f9428bcbf4586e1447425f01e9f2
HubNBr <79989810+HubNBr@users.noreply.github.com>
2022-11-16 11:56:06 +0100

    completed basic detail page

--- src/client/detail.js
+++ src/client/detail.js
@@ -23,10 +23,10 @@ function render(div, search) {
     .request(query(search))
     .then((response) => {
       response.results.bindings.forEach((item) => {
-        let detail = document.createElement('div');
+        //let detail = document.createElement('div');
         let subCol = document.createElement('div');
         let mainCol = document.createElement('div');
-        detail.classList.add('detail');
+        //detail.classList.add('detail');
         subCol.classList.add('detail--sub');
         mainCol.classList.add('detail--main');
 
@@ -40,21 +40,55 @@ function render(div, search) {
         img_container.appendChild(img);
         img_container.classList.add('detail__img-container');
         subCol.appendChild(img_container);
+
+        if(item.countries.value) {
+            let countries = document.createElement('div');
+            let countriesTitle = document.createElement('h2');
+            let countriesNames = document.createElement('p');
+            countriesTitle.innerText = "Pays lié(s)";
+            countriesNames.innerText = item.countries.value;
+            countries.appendChild(countriesTitle);
+            countries.appendChild(countriesNames);
+            subCol.appendChild(countries);
+        }
+
+        if(item.regions.value) {
+            let regions = document.createElement('div');
+            let regionsTitle = document.createElement('h2');
+            let regionsNames = document.createElement('p');
+            regionsTitle.innerText = "Régions liée(s)";
+            regionsNames.innerText = item.regions.value;
+            regions.appendChild(regionsTitle);
+            regions.appendChild(regionsNames);
+            subCol.appendChild(regions);
+        }
+
+        if(item.ingredients.value) {
+            let ingredients = document.createElement('div');
+            let ingredientsTitle = document.createElement('h2');
+            let ingredientsNames = document.createElement('p');
+            ingredientsTitle.innerText = "Ingrédient(s)";
+            ingredientsNames.innerText = item.ingredients.value;
+            ingredients.appendChild(ingredientsTitle);
+            ingredients.appendChild(ingredientsNames);
+            subCol.appendChild(ingredients);
+        }
         
 
-        let name = document.createElement('h2');
+        let name = document.createElement('h1');
         name.classList.add('detail__name');
         name.innerText = item.name.value;
-        subCol.appendChild(name);
+        mainCol.appendChild(name);
 
         let desc = document.createElement('p');
         desc.classList.add('detail__desc');
         desc.innerText = item.desc.value;
         mainCol.appendChild(desc);
 
-        detail.append(subCol);
-        detail.append(mainCol);
-        div.appendChild(detail);
+        /* detail.append(subCol);
+        detail.append(mainCol); */
+        div.appendChild(mainCol);
+        div.appendChild(subCol);
       });
     })
     .catch((error) => {
--- src/client/results.js
+++ src/client/results.js
@@ -11,9 +11,12 @@ window.addEventListener('load', () => {
 });
 
 const query = (search) => `
-SELECT ?name, ?img, ?desc, ?country, GROUP_CONCAT(?region;SEPARATOR=", ") AS ?regions WHERE {
-  ?dish a dbo:Food; rdfs:label ?name; dbo:thumbnail ?img; rdfs:comment ?desc; dbo:country ?country; dbo:region ?region.
-  FILTER(regex(?name, "${search}", "i") && langMatches(lang(?name), "FR") && langMatches(lang(?desc), "FR"))
+SELECT ?name, ?img, ?desc, GROUP_CONCAT(DISTINCT ?countryName; SEPARATOR=", ") AS ?countries, GROUP_CONCAT(DISTINCT ?regionName; SEPARATOR=", ") AS ?regions, GROUP_CONCAT(DISTINCT ?ingredientName; SEPARATOR=", ") AS ?ingredients WHERE {
+    ?dish a dbo:Food; rdfs:label ?name; dbo:thumbnail ?img; rdfs:comment ?desc.
+    OPTIONAL {?dish dbp:country ?country. ?country rdfs:label ?countryName. FILTER(langMatches(lang(?countryName), "FR"))}
+    OPTIONAL {?dish dbp:region ?region. ?region rdfs:label ?regionName. FILTER(langMatches(lang(?regionName), "FR"))}
+    OPTIONAL {?dish dbo:ingredient ?ingredient. ?ingredient rdfs:label ?ingredientName. FILTER(langMatches(lang(?ingredientName), "FR"))}
+    FILTER(regex(?name, "${search}", "i") && langMatches(lang(?name), "FR") && langMatches(lang(?desc), "FR"))
 }`;
 
 function render(div, search) {
