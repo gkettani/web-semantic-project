e9c33317876f97311d70f5cc2f7dc3f25ba9a979
HubNBr <79989810+HubNBr@users.noreply.github.com>
2022-11-16 19:40:03 +0100

    refactored queries in libQuery.js

--- src/client/detail.js
+++ src/client/detail.js
@@ -1,4 +1,5 @@
 import webService from './request.js';
+import { basicQuery } from './libQuery.js';
 
 
 window.addEventListener('load', () => {
@@ -9,18 +10,9 @@ window.addEventListener('load', () => {
   render(div, search)
 });
 
-const query = (search) => `
-SELECT ?name, ?img, ?desc, GROUP_CONCAT(DISTINCT ?countryName; SEPARATOR=", ") AS ?countries, GROUP_CONCAT(DISTINCT ?regionName; SEPARATOR=", ") AS ?regions, GROUP_CONCAT(DISTINCT ?ingredientName; SEPARATOR=", ") AS ?ingredients WHERE {
-    ?dish a dbo:Food; rdfs:label ?name; dbo:thumbnail ?img; rdfs:comment ?desc.
-    OPTIONAL {?dish dbp:country ?country. ?country rdfs:label ?countryName. FILTER(langMatches(lang(?countryName), "FR"))}
-    OPTIONAL {?dish dbp:region ?region. ?region rdfs:label ?regionName. FILTER(langMatches(lang(?regionName), "FR"))}
-    OPTIONAL {?dish dbo:ingredient ?ingredient. ?ingredient rdfs:label ?ingredientName. FILTER(langMatches(lang(?ingredientName), "FR"))}
-    FILTER(regex(?name, "${search}", "i") && langMatches(lang(?name), "FR") && langMatches(lang(?desc), "FR"))
-}`;
-
 function render(div, search) {
   webService
-    .request(query(search))
+    .request(basicQuery(search))
     .then((response) => {
       response.results.bindings.forEach((item) => {
         //let detail = document.createElement('div');
