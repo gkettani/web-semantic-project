ac4ff61a718f92b2430f850f2fd84a1ebc9af75d
Yahia Bellamine <92577857+YahiaBellamine@users.noreply.github.com>
2022-11-18 10:54:30 +0100

    Add ingredientQuery and world map and fix image on error

--- src/client/results.js
+++ src/client/results.js
@@ -1,6 +1,6 @@
 import webService from './request.js';
 import { truncate, redirect } from './utils.js';
-import { basicQuery, countryQuery } from './libQuery.js';
+import { basicQuery, countryQuery, ingredientQuery } from './libQuery.js';
 
 
 window.addEventListener('load', () => {
@@ -22,6 +22,11 @@ function render(div, search) {
   let cont2Title = document.createElement('h1');
   cont2Title.innerText = "Résultats par cuisine régionale";
   
+  let container3 = document.createElement('div');
+  container3.classList.add('result-container');
+  let cont3Title = document.createElement('h1');
+  cont3Title.innerText = "Résultats par ingrédient";
+  
   webService
     .request(basicQuery(search))
     .then((response) => {
@@ -29,7 +34,8 @@ function render(div, search) {
         let result = document.createElement('div');
         let img_container = document.createElement('div');
         let img = document.createElement('img');
-        img.src = item.img.value || 'https://via.placeholder.com/150';
+        img.src = item.img.value;
+        img.onerror = "this.error=null;this.src='../rsrc/unknownDish.png';"
         img.alt = item.name.value;
         img.classList.add('result__img');
         img_container.appendChild(img);
@@ -66,7 +72,8 @@ function render(div, search) {
         let result = document.createElement('div');
         let img_container = document.createElement('div');
         let img = document.createElement('img');
-        img.src = item.img.value || 'https://via.placeholder.com/150';
+        img.src = item.img.value;
+        img.onerror = function() { this.error=null;this.src='../rsrc/unknownDish.png'; }
         img.alt = item.name.value;
         img.classList.add('result__img');
         img_container.appendChild(img);
@@ -90,6 +97,44 @@ function render(div, search) {
       {
         div.appendChild(cont2Title);
         div.appendChild(container2);
+      }
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+
+    webService
+    .request(ingredientQuery(search))
+    .then((response) => {
+      response.results.bindings.forEach((item) => {
+        let result = document.createElement('div');
+        let img_container = document.createElement('div');
+        let img = document.createElement('img');
+        img.src = item.img.value;
+        img.onerror = function() { this.error=null;this.src='../rsrc/unknownDish.png'; }
+        img.alt = item.name.value;
+        img.classList.add('result__img');
+        img_container.appendChild(img);
+        img_container.classList.add('result__img-container');
+        result.appendChild(img_container);
+        result.classList.add('result');
+        let name = document.createElement('h2');
+        name.classList.add('result__name');
+        name.innerText = item.name.value;
+        result.appendChild(name);
+        let desc = document.createElement('p');
+        desc.classList.add('result__desc');
+        desc.innerText = truncate(item.desc.value, 100);
+        result.appendChild(desc);
+        result.addEventListener('click', () => {
+          redirect(`detail`, `search`, item.name.value);
+        });
+        container3.appendChild(result);
+      });
+      if(container3.innerText)
+      {
+        div.appendChild(cont3Title);
+        div.appendChild(container3);
       }
     })
     .catch((error) => {
